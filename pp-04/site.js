2713b7c6dddd3ed2b04dff340ae2bdb15267b876
Natalie Herrmann <nherrma1@hawk.iit.edu>
2018-11-11 01:01:31 -0600

    Add IIFE to check for broswer support and DOM loaded

--- pp-04/site.js
+++ pp-04/site.js
@@ -1,4 +1,13 @@
+'use strict';
 function ringDoorbell() {
   var doorbell = new Audio('media/doorbell.mp3');
   doorbell.play();
 }
+(function(){
+  if (typeof(document.querySelector)==="undefined") {
+    return;
+  }
+  document.addEventListener('DOMContentLoaded', function() {
+
+  });
+})();
