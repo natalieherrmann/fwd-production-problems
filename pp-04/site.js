1344d1fc51e4437768db85a6ff679e781fd9930a
Natalie Herrmann <nherrma1@hawk.iit.edu>
2018-11-11 01:07:55 -0600

    Use JS to replace audio HTML with clickable button

--- pp-04/site.js
+++ pp-04/site.js
@@ -7,7 +7,13 @@ function ringDoorbell() {
   if (typeof(document.querySelector)==="undefined") {
     return;
   }
-  document.addEventListener('DOMContentLoaded', function() {
-
+  document.addEventListener("DOMContentLoaded", function() {
+    var button = document.createElement("a");
+    var button_text = document.createTextNode("Ring the Doorbell");
+    var figure = document.querySelector(".audio");
+    var figureParent = figure.parentNode;
+    button.setAttribute("href", "");
+    button.appendChild(button_text);
+    figureParent.replaceChild(button, figure);
   });
 })();
